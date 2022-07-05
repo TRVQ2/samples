b632a3956f99e675521a188a12190175bcb3c9e3
RuslanTomashevsky <68241295+RuslanTomashevsky@users.noreply.github.com>
2022-07-05 17:48:31 +0300

    Sportsbook tools for BO configuration

--- sportsbook/CheckMarketTypeAttributes/check.py
+++ sportsbook/CheckMarketTypeAttributes/check.py
@@ -15,6 +15,7 @@ for item in parameters:
             item['translations'] = []
         elif 'translations' not in item or item['translations'] is None:
             item['translations'] = [{
+                "entityId": item['entity']['id'],
                 "fieldName": "selectionNameTemplate",
                 "entityType": "MARKET_TYPE",
                 "lang": "original",
@@ -22,13 +23,31 @@ for item in parameters:
                 "order": 0,
                 "parentOrder": 0
             }]
+        else:
+            # clean up originals
+            for i in item['translations'][::-1]:
+                if i['lang'] == 'original':
+                    item['translations'].remove(i)
+
+            item['translations'].append({
+                "entityId": item['entity']['id'],
+                "fieldName": "selectionNameTemplate",
+                "entityType": "MARKET_TYPE",
+                "lang": "original",
+                "value": selectionNameTemplate,
+                "order": 0,
+                "parentOrder": 0
+            })
     # remove Bet Builder attributes
     if 'attributes' in item['entity']:
         attributes = item['entity']['attributes']
         if (attributes is not None and len(attributes) > 0):
             for i in attributes[::-1]:
-                if i['type'] in ('participantType', 'periodType', 'team'):
+                if i['type'] in ('participantType', 'team'):
                     attributes.remove(i)
+                elif i['type'] == 'periodType':
+                    if i['value'] not in ("10MIN", "FH", "SH", "FT"):
+                        attributes.remove(i)
 
 json_dump = json.dumps(parameters, indent=2)
 open("out_" + file_name, 'w').write(json_dump)
---
+++ sportsbook/coupons/coupon_update_market_filters.py
@@ -0,0 +1,76 @@
+import requests
+import os
+import json
+import string
+
+
+def get_coupons_list(url):
+    response = requests.get(url, allow_redirects=False).text
+    return json.loads(response)["data"]
+
+
+def update_coupons_list(url, list):
+    failed_cases = []
+    for i in list:
+        uid = i["uid"]
+        print(f"Processing coupon '{uid}'. Sending request...")
+        i.pop("uid")
+        response = requests.put(
+            f"{url}/{uid}",
+            json={"changeSet": i},
+            allow_redirects=False
+        )
+        status_code = response.status_code
+        print(f"Status code: {status_code}")
+        if status_code != 200:
+            failed_cases.append({"uid": uid, "status_code": status_code})
+
+    print("Failed cases:\n" + '\n'.join(str(x) for x in failed_cases))
+    return failed_cases
+
+
+def update_coupons(list):
+    for i in list:
+        if 'marketFilterId' not in i:
+            i['marketFilterId'] = None
+            i['marketFilterIds'] = []
+        elif i['marketFilterId'] is None:
+            i['marketFilterIds'] = []
+        elif 'marketFilterIds' not in i \
+            or i['marketFilterIds'] is None \
+                or i['marketFilterIds'] == []:
+            i['marketFilterIds'] = [i['marketFilterId']]
+    return list
+
+
+def check_market_filters(list):
+    for i in list:
+        if 'marketFilterId' in i and i['marketFilterId'] is not None:
+            print(f"uid={i['uid']}, marketFilterId={i['marketFilterId']}")
+            print(f"uid={i['uid']}, marketFilterIds={i['marketFilterIds']}")
+    return
+
+
+def update_coupon_market_filters(brand, env, session):
+    parameters = json.loads(open('config.json').read())
+    brands = parameters['brands']
+    envs = parameters['envs']
+    get_url = parameters['get_all_coupons_url'] \
+        .format(envs[env], brands[brand])
+    coupons = update_coupons(get_coupons_list(get_url))
+    post_url = parameters['update_coupon_url'] \
+        .format(envs[env], brands[brand])
+    # update_coupons_list(post_url, coupons)
+    check_market_filters(coupons)
+
+
+def main():
+    brands = ["vb", "vbuk", "uk", "ng", "nl"]
+    env = "dev"
+    session = ''
+    for i in brands:
+        update_coupon_market_filters(i, env, session)
+
+
+if __name__ == "__main__":
+    main()
