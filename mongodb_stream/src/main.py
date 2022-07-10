1865af187c82fa1f7aa5005e63bad67e48c9f951
Ruslan Tomashevsky <Ruslan.Tomashevsky@gmail.com>
2022-07-10 22:39:18 +0300

    MongoDB - listing all data in collection

--- mongodb_stream/src/main.py
+++ mongodb_stream/src/main.py
@@ -6,6 +6,8 @@ from bson.json_util import dumps
 parameters = json.loads(open('../env_config.json').read())
 connection_str = parameters['CHANGE_STREAM_DB']
 client = pymongo.MongoClient(connection_str)
+# for x in client.changestream.collection.find():  # listing all data
+#    print(x)
 change_stream = client.changestream.collection.watch()
 for change in change_stream:
     print(dumps(change), '\n')
