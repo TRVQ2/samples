f130f3b3ba58ee69e4132f6a382c6048d335c510
RuslanTomashevsky <68241295+RuslanTomashevsky@users.noreply.github.com>
2022-07-12 20:43:01 +0300

    MongoDB Updater and Listener. Working sample using DEV DB

--- mongodb_stream/src/main.py
+++ mongodb_stream/src/main.py
@@ -3,11 +3,11 @@ import pymongo
 import json
 from bson.json_util import dumps
 
+
 parameters = json.loads(open('../env_config.json').read())
-connection_str = parameters['CHANGE_STREAM_DB']
-client = pymongo.MongoClient(connection_str)
-# for x in client.changestream.collection.find():  # listing all data
-#    print(x)
-change_stream = client.changestream.collection.watch()
+client = pymongo.MongoClient(parameters['CONNECTION_STR'])
+db = client[parameters['DB_NAME']]
+collection = db[parameters['COLLECTION']]
+change_stream = collection.watch()
 for change in change_stream:
     print(dumps(change), '\n')
--- mongodb_stream/src/send_message.py
+++ mongodb_stream/src/send_message.py
@@ -3,6 +3,11 @@ import pymongo
 import json
 
 parameters = json.loads(open('../env_config.json').read())
-connection_str = parameters['CHANGE_STREAM_DB']
-client = pymongo.MongoClient(connection_str)
-print(client.changestream.collection.insert_one({"hello": "TRV"}).inserted_id)
+client = pymongo.MongoClient(parameters['CONNECTION_STR'])
+db = client[parameters['DB_NAME']]
+collection = db[parameters['COLLECTION']]
+record = collection.find_one()
+print(record)
+# print(collection.insert_one({"test": "test"}).inserted_id)
+# filter = {"_id": record["_id"]}
+# collection.update_one(filter, {"$set": {"test": None}})
